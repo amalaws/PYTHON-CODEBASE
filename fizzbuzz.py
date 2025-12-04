308c07cc329473e1524ea5f910ee7c11b17337a6
Amal Raj <amalraj@Amals-MacBook-Air.local>
2025-12-04 11:29:00 +0530

    commit this new file

---
+++ fizzbuzz.py
@@ -0,0 +1,11 @@
+stri = int(input("Enter the number: "))
+
+for i in range(1,stri+1):
+        if i%3 == 0 and i%5 == 0:
+          print("FizzBuzz")
+        elif i%3 == 0:
+          print("Fizz")
+        elif i%5 == 0:
+          print("Buzz")
+else:
+    print(stri)
